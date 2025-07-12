5583c6a9ccbb72da0ec27f617b6e7707ec44b7e8
Ajayberli <ajayberli70@gmail.com>
2025-07-12 14:00:33 +0530

    commit

---
+++ python/basic.py
@@ -0,0 +1,125 @@
+print("Hey, everyone how are you? ")
+a = "hello"
+num = 10 
+# @ = 4
+#  2= 2
+age = 22
+age =23
+print(age)
+print(age)
+name ="ajay kumar"
+print(name)
+print("name dtypes: " ,type(name))
+print("len of name: ",len(name))
+### indexing
+print(name[0])
+print(name[2])
+print(name[len(name)-1])
+
+
+### operation 
+name = "ajay kumar"
+upper_case =name.upper() ### upper function ka used upper case k liye kiya jaata h
+# print(upper_case)
+# lower_last= name=last.lower(
+ #print(last_lower_name)
+
+print(name.count("r"))
+name = "ajay kumar"
+last ="sharma"
+#print(name.title())
+#print(nmae.capitalize())
+print(name+last)
+
+
+# print(lst[0:5])
+# lst =[1,2,3,4,5,6,7,]
+# lst.pop()
+# print(lst)
+# lst.insert(0,1000)
+# print(lst)
+# copy_lst =lst.copy()
+#lst.reverse(
+#lst.remove(2,3)
+# print(lst)
+#lst.sort()
+# tpl =(1,2,3,4,5,6)
+# print (" max of tpl ", max(tpl))
+# print (" max of tpl ",min(tpl))
+# sum(tpl)
+   
+
+#    my_dict = {
+#       "name":"ajay"
+#     "class":"btech"
+#     "roll no":"23ru310004"
+#     "address":"jaipur"
+#    }
+# print("my first dic :",(my_dict))
+# print("len dic :",len(my_dict))
+
+# print("type of dic :",type(my_dict))
+# print(my_dict['nmae'])
+# print(my_dictt['class'])
+# print(my_dict['sddress'])
+a=2
+print(a)
+
+
+
+
+
+
+
+
+
+
+# print("type of dic :",type(my_dict))
+# print(my_dict['name'])
+# print(my_dictt['class'])
+# print(my_dict['address'])
+# tast >>>> update function
+#
+# get function >>> 5 or []
+
+# my_dict.key()
+# my_dict.value()
+# my_dict.item()
+  print( my_dict.key())
+printmy_dict.value()()
+print(my_dict.item())
+# my_dict.clear()
+# print(my_dict)
+
+a = input("enter a number")
+input("enter a number")
+
+
+# type casting 
+# a = int(input"enter a number")
+
+# b = int(input("enter a second number"))
+# print(a*b)
+
+# lst =[1,2,3,4,5,5,58]
+# print("this is my list:",lst)
+# print("type of tuple:,type(tpl)")
+# tpl = tuple(lst)
+# print("this is my tuple:",tpl)
+# print("type of tuple:,type(tpl)")
+
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>set>>>>>>>>>>>>>>>>>>>>>>>>
+ 
+#  my_set =
+# tuple
+# list
+# set
+# dict
+
+my_set ={1,2,3,4,5,6,7, "jai"}
+print("this is my first set :",my_set)
+print("len of my set :",len(my_set))
+print("type of my set :",type(my_set))
---
+++ python/list.py
@@ -0,0 +1,98 @@
+
+lst =[1,2,3,4,5,6,7,] ### >>>>>list
+# array of list >>
+# mutable
+# duplicated value 
+#  ## herto
+# order >>>>>
+# array >>>>> list =[1,2,3,4,5,6,]
+# array
+print(lst)
+print(" my first letter:", lst)
+print( "len of first:", lst)
+print("my list name:", len(lst))
+
+# print(lst[0:5])
+# lst =[1,2,3,4,5,6,7,]
+# lst.pop()
+# print(lst)
+# lst.insert(0,1000)
+# print(lst)
+# copy_lst =lst.copy()
+#lst.reverse(
+#lst.remove(2,3)
+# print(lst)
+#lst.sort()
+# lst =[1,2,3,4,5,6,7,]
+#print(lst.count(2))
+# print(lst)
+
+
+
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>> tuple<<<<<
+# tuple =
+# tpl = (1,2,3,4,5,585,5, "ajay",2.3)
+# print(" my first tuple:",tpl)
+# print(" len of tuple:",len(tpl))
+# print(" type of tuple:",type(tpl))
+# print(tpl[0])
+# print(tpl[2])
+# print(tpl[0:6])
+
+# a = 1,2,3,4,5,6,
+# print(a)
+# print(type(a))
+# print(len(a))
+# a,b,c, =(1,2,3,)
+# print(a)
+# print(b)
+# print(c)
+
+
+
+ print(lst[0:5])
+# lst =[1,2,3,4,5,6,7,]
+# lst.pop()
+# print(lst)
+# lst.insert(0,1000)
+# print(lst)
+# copy_lst =lst.copy()
+#lst.reverse(
+#lst.remove(2,3)
+# print(lst)
+#lst.sort()
+tpl =(1,2,3,4,5,6)
+print (" max of tpl ", max(tpl))
+print (" max of tpl ",min(tpl))
+sum(tpl)
+   
+
+   my_dict = {
+      "name":"ajay"
+    "class":"btech"
+    "roll no":"23ru310004"
+    "address":"jaipur"
+   }
+# print("my first dic :",(my_dict))
+# print("len dic :",len(my_dict))
+
+# print("type of dic :",type(my_dict))
+# print(my_dict['name'])
+# print(my_dictt['class'])
+# print(my_dict['address'])
+# tast >>>> update function
+#
+# get function >>> 5 or []
+
+# my_dict.key()
+# my_dict.value()
+# my_dict.item()
+  print( my_dict.key())
+printmy_dict.value()()
+print(my_dict.item())
+
+copy_dict = my_dict.copy()
+print(copy_dict)
